cb182c79cd885721f92093b60b9da8033385d7fe
ACPLMaverick <MaverickCCCP@Gmail.com>
2017-03-07 14:19:21 +0100

    [+] Test01

---
+++ Test01/src/test/java/Test01/AppTest.java
@@ -0,0 +1,30 @@
+package Test01;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest()
+    {
+
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
